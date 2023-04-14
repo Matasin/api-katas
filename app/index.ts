6b8cb2145308898a122fee9c9f01dcd383735851
Patryk Laciak <matasinqwer@gmail.com>
2023-04-14 18:52:18 +0200

    swagger

--- app/index.ts
+++ app/index.ts
@@ -1,18 +1,18 @@
 import http from 'http';
-import express from 'express';
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
 import { ApolloServer } from 'apollo-server-express';
 import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
-
 import swaggerUi from 'swagger-ui-express';
-import swaggerSpec from './swagger';
+import swaggerJsdoc from 'swagger-jsdoc'
 
 import Schema from './Schema';
 import Resolvers from './Resolvers';
+import { users } from './dataset';
+import { User } from './types';
 
 async function startApolloServer(schema: any, resolvers: any) {
   const app = express();
-
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
   const httpServer = http.createServer(app);
   const server = new ApolloServer({
@@ -33,3 +33,99 @@ async function startApolloServer(schema: any, resolvers: any) {
 }
 
 startApolloServer(Schema, Resolvers);
+
+// - REST - 
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/users', (req: Request, res: Response) => {
+  res.json(users);
+});
+
+app.get('/users/:id', (req: Request, res: Response) => {
+  const id = req.params.id;
+  const user = users.find((u) => u.id === id);
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).send('User not found');
+  }
+});
+
+app.post('/users', (req: Request, res: Response) => {
+  const { name, email, isVerified, data } = req.body;
+  if (!name || !email || isVerified === undefined || !data) {
+    res.status(400).send('Name, email, isVerified, and data are required');
+    return;
+  }
+  const newUser: User = {
+    id: String(users.length + 1),
+    name,
+    email,
+    isVerified,
+    data,
+  };
+  users.push(newUser);
+  res.json(newUser);
+});
+
+app.put('/users/:id', (req: Request, res: Response) => {
+  const id = req.params.id;
+  const { name, email, isVerified, data } = req.body;
+  const user = users.find((u) => u.id === id);
+  if (!user) {
+    res.status(404).send('User not found');
+    return;
+  }
+  if (name) {
+    user.name = name;
+  }
+  if (email) {
+    user.email = email;
+  }
+  if (isVerified !== undefined) {
+    user.isVerified = isVerified;
+  }
+  if (data) {
+    user.data = data;
+  }
+  res.json(user);
+});
+
+app.delete('/users/:id', (req: Request, res: Response) => {
+  const id = req.params.id;
+  const userIndex = users.findIndex((u) => u.id === id);
+  if (userIndex === -1) {
+    res.status(404).send('User not found');
+    return;
+  }
+  users.splice(userIndex, 1);
+  res.sendStatus(204);
+});
+
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'My API',
+      version: '1.0.0',
+    },
+    servers: [
+      {
+        url: `http://localhost:${PORT}`,
+      },
+    ],
+  },
+  apis: ['swagger.yml'],
+};
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
